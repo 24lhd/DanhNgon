c76fd55f9e42ff39f718eb0835ad7155433e1624
24duong@gmail.com <24duong@gmail.com>
2017-01-19 07:57:37 +0700

    xong giao dien tab

---
+++ app/src/main/java/com/d/activity/MainActivity.java
@@ -0,0 +1,101 @@
+package com.d.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.d.danhngon.R;
+import com.d.fragment.FmDanhNgon;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, Serializable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    /**
+     * frangment custem
+     */
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        public static PlaceholderFragment newInstance(int sectionNumber) {
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState) {
+////            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
+////            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+////            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            RecyclerView recyclerView=new RecyclerView(getActivity());
+//            return recyclerView;
+//        }
+//    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+         if (id == R.id.danh_ngon) {
+             android.support.v4.app.FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+             FmDanhNgon fmDanhNgon=new FmDanhNgon();
+             Bundle args = new Bundle();
+            args.putSerializable("fm", MainActivity.this);
+             fmDanhNgon.setArguments(args);
+             transaction.replace(R.id.frame_fm, fmDanhNgon);
+             transaction.commit();
+             Log.e("faker","int");
+
+        } else if (id == R.id.chia_se) {
+             android.support.v4.app.FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+             transaction.replace(R.id.frame_fm, new FmDanhNgon.PlaceholderFragment());
+             transaction.commit();
+             Log.e("faker","int");
+        } else if (id == R.id.more_app) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/d/fragment/FmDanhNgon.java
@@ -0,0 +1,148 @@
+package com.d.fragment;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.d.activity.MainActivity;
+import com.d.danhngon.R;
+
+/**
+ * Created by d on 19/01/2017.
+ */
+
+public class FmDanhNgon extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private MainActivity mainActivity;
+//    public static FmDanhNgon newInstance(int sectionNumber) {
+//        FmDanhNgon fragment = new FmDanhNgon();
+//        Bundle args = new Bundle();
+//        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private TabLayout tabLayout;
+    private LayoutInflater inflater;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+            this.inflater=inflater;
+            View rootView = inflater.inflate(R.layout.layout_danh_ngon, container, false);
+          mainActivity= (MainActivity) getArguments().getSerializable("fm");
+//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//        RecyclerView recyclerView=new RecyclerView(getActivity());
+
+        tabLayout= (TabLayout) rootView.findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) rootView.findViewById(R.id.container);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(mainActivity.getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setSelected(false);
+        Log.e("faker",""+tabLayout.getTabCount());
+        for (int i = 0; i <tabLayout.getTabCount(); i++) {
+            View view=inflater.inflate(R.layout.tab_custem,null);
+            TextView tv= (TextView) view.findViewById(R.id.tv_tab);
+            tv.setText(""+i);
+            tabLayout.getTabAt(i).setCustomView(tv);
+            Log.e("faker",""+i);
+        }
+
+        tabLayout.setSelectedTabIndicatorHeight(0);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+            }
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.tab_selecter));
+            }
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+            }
+        });
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return PlaceholderFragment.newInstance(position);
+        }
+        @Override
+        public int getCount() {
+            return 10;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+                default:
+                    return "SECTION 4";
+            }
+        }
+    }
+    public static class PlaceholderFragment extends Fragment {
+
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            RecyclerView recyclerView=new RecyclerView(getActivity());
+//            recyclerView.setBackgroundColor(Color.BLACK);
+            return rootView;
+        }
+    }
+}
