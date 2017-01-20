8aa1c697dd919034b9825376662fa0e3d3424abd
24duong@gmail.com <24duong@gmail.com>
2017-01-20 23:32:51 +0700

    them chinh sau giao din fix loi hien thi viewpager

--- app/src/main/java/com/d/activity/MainActivity.java
+++ app/src/main/java/com/d/activity/MainActivity.java
@@ -34,6 +34,7 @@ import com.d.task.TaskGetDanhNgon;
 
 import java.util.ArrayList;
 
+import duong.AppLog;
 import duong.ChucNangPhu;
 import duong.Communication;
 import duong.DiaLogThongBao;
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements
 
     public static final String LIST_DATA = "list_data";
     public static final String THIS = "this";
+    public static final String STATE_UI = "state_ui";
     private Toolbar toolbar;
     private ArrayList<Category> categories;
     private ArrayList<DanhNgon> danhNgons;
@@ -54,15 +56,18 @@ public class MainActivity extends AppCompatActivity implements
     private TabLayout tabUI;
     private FmDanhNgon fmDanhNgon;
     private FrameLayout frameLayout;
+    private AppLog appLog;
+    private Window window;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showDialogLoad(this, "Đang khởi tạo dữ liệu...");
-         duLieu = new DuLieu(this);
-            initData();
+        initData();
     }
     private void initData() {
+        duLieu = new DuLieu(this);
         try {
                 if (duLieu.checkDB()){
                     ChucNangPhu.showLog("if "+duLieu.checkDB());
@@ -141,9 +146,15 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void initView() {
+        appLog=new AppLog();
+        appLog.openLog(this,STATE_UI);
+         window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         frameLayout= (FrameLayout) findViewById(R.id.frame_fm);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        fmDanhNgon=new FmDanhNgon();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -151,6 +162,11 @@ public class MainActivity extends AppCompatActivity implements
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        setUI(appLog.getValueByName(this,STATE_UI,"StatusBar"),
+                appLog.getValueByName(this,STATE_UI,"toolbar"),
+                appLog.getValueByName(this,STATE_UI,"shape_yes"),
+                appLog.getValueByName(this,STATE_UI,"shape_no"),
+                appLog.getValueByName(this,STATE_UI,"frameLayout"));
     }
 
     @Override
@@ -173,70 +189,74 @@ public class MainActivity extends AppCompatActivity implements
         } else if (id == R.id.more_app) {
 
         }else if (id == R.id.thay_doi_ui) {
-            LayoutInflater layoutInflater=getLayoutInflater();
-
-            View view=layoutInflater.inflate(R.layout.layout_select_ui,null);
-             tabUI= (TabLayout) view.findViewById(R.id.tab_ui_layout);
-            tabUI.setTabMode(TabLayout.MODE_SCROLLABLE);
-            tabUI.setTabGravity(TabLayout.GRAVITY_FILL);
-            tabUI.setSelectedTabIndicatorHeight(0);
-
-            TabLayout.Tab tabStatus=tabUI.newTab();
-            tabStatus.setText("Status Bar");
-            tabUI.addTab(tabStatus);
-
-            TabLayout.Tab tabBar=tabUI.newTab();
-            tabBar.setText("Bar");
-            tabUI.addTab(tabBar);
-
-            TabLayout.Tab tab=tabUI.newTab();
-            tab.setText("Tab unselect");
-            tabUI.addTab(tab);
-
-            TabLayout.Tab tabCategory=tabUI.newTab();
-            tabCategory.setText("Tab select");
-            tabUI.addTab(tabCategory);
-
-            TabLayout.Tab tabBg=tabUI.newTab();
-            tabBg.setText("Background");
-            tabUI.addTab(tabBg);
-            LayoutInflater inflater=getLayoutInflater();
-            for (int i = 0; i < tabUI.getTabCount(); i++) {
-                View viewTab=inflater.inflate(R.layout.tab_custem,null);
-                TextView tv2= (TextView) viewTab.findViewById(R.id.tv_tab);
-                tv2.setText(tabUI.getTabAt(i).getText());
-                tabUI.getTabAt(i).setCustomView(tv2);
-            }
-            tabUI.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                @Override
-                public void onTabSelected(TabLayout.Tab tab) {
-                     tabUISelect=tab.getPosition();
-                    tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
-                }
-                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                @Override
-                public void onTabUnselected(TabLayout.Tab tab) {
-                    tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
-                }
-                @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-                @Override
-                public void onTabReselected(TabLayout.Tab tab) {
-                    tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
-                }
-            });
-            tabUI.getTabAt(0).select();
-            tabUISelect=0;
-            DiaLogThongBao.createDiaLogView(this, view, null , null, null, getResources().getColor(R.color.colorPrimary),null, null).show();
-        }
+            showDialogSetUI();
+           }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    private void showDialogSetUI() {
+        LayoutInflater layoutInflater=getLayoutInflater();
+        View view=layoutInflater.inflate(R.layout.layout_select_ui,null);
+        tabUI= (TabLayout) view.findViewById(R.id.tab_ui_layout);
+        tabUI.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabUI.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabUI.setSelectedTabIndicatorHeight(0);
+
+        TabLayout.Tab tabStatus=tabUI.newTab();
+        tabStatus.setText("Status Bar");
+        tabUI.addTab(tabStatus);
+
+        TabLayout.Tab tabBar=tabUI.newTab();
+        tabBar.setText("Bar");
+        tabUI.addTab(tabBar);
+
+        TabLayout.Tab tab=tabUI.newTab();
+        tab.setText("Tab unselect");
+        tabUI.addTab(tab);
+
+        TabLayout.Tab tabCategory=tabUI.newTab();
+        tabCategory.setText("Tab select");
+        tabUI.addTab(tabCategory);
+
+        TabLayout.Tab tabBg=tabUI.newTab();
+        tabBg.setText("Background");
+        tabUI.addTab(tabBg);
+        LayoutInflater inflater=getLayoutInflater();
+        for (int i = 0; i < tabUI.getTabCount(); i++) {
+            View viewTab=inflater.inflate(R.layout.tab_custem,null);
+            TextView tv2= (TextView) viewTab.findViewById(R.id.tv_tab);
+            tv2.setText(tabUI.getTabAt(i).getText());
+            tabUI.getTabAt(i).setCustomView(tv2);
+        }
+        tabUI.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tabUISelect=tab.getPosition();
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+            }
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
+            }
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                tab.getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+            }
+        });
+        tabUI.getTabAt(0).select();
+        tabUISelect=0;
+        DiaLogThongBao.createDiaLogView(this, view, null , null, null, getResources().getColor(R.color.colorPrimary),null, null).show();
+
+    }
+
     private void setViewDanhNgon() {
         android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-         fmDanhNgon=new FmDanhNgon();
+        fmDanhNgon=new FmDanhNgon();
         transaction.replace(R.id.frame_fm, fmDanhNgon);
         transaction.commit();
     }
@@ -245,51 +265,32 @@ public class MainActivity extends AppCompatActivity implements
         switch (tabUISelect){
             case 1:
                 toolbar.setBackgroundColor(floatingActionButton.getBackgroundTintList().getDefaultColor());
+                appLog.putValueByName(this,STATE_UI,"toolbar",""+floatingActionButton.getBackgroundTintList().getDefaultColor());
                 break;
             case 2:
                 GradientDrawable shape_no= (GradientDrawable) getResources().getDrawable(R.drawable.shape_no);
                 shape_no.setColor(floatingActionButton.getBackgroundTintList().getDefaultColor());
+                appLog.putValueByName(this,STATE_UI,"shape_no",""+floatingActionButton.getBackgroundTintList().getDefaultColor());
+                loadColorTab();
                 break;
             case 3:
                 GradientDrawable shape_yes= (GradientDrawable) getResources().getDrawable(R.drawable.shape_yes);
                 shape_yes.setColor(floatingActionButton.getBackgroundTintList().getDefaultColor());
+                appLog.putValueByName(this,STATE_UI,"shape_yes",""+floatingActionButton.getBackgroundTintList().getDefaultColor());
+                loadColorTab();
                 break;
             case 4:
                 frameLayout.setBackgroundColor(floatingActionButton.getBackgroundTintList().getDefaultColor());
+                appLog.putValueByName(this,STATE_UI,"frameLayout",""+floatingActionButton.getBackgroundTintList().getDefaultColor());
                 break;
             default:
-                Window window = this.getWindow();
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     window.setStatusBarColor(floatingActionButton.getBackgroundTintList().getDefaultColor());
+                    appLog.putValueByName(this,STATE_UI,"StatusBar",""+floatingActionButton.getBackgroundTintList().getDefaultColor());
                 }else {
                     Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
                 }
                 break;
-        }
-
-        for (int i = 0; i < tabUI.getTabCount(); i++) {
-            if (tabUI.getTabAt(i).isSelected()){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    tabUI.getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
-                else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
-            }else{
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    tabUI.getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
-                else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
-            }
-        }
-        for (int i = 0; i < fmDanhNgon.getTabLayout().getTabCount(); i++) {
-            if (fmDanhNgon.getTabLayout().getTabAt(i).isSelected()){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    fmDanhNgon.getTabLayout().getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
-                else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
-            }else{
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                    fmDanhNgon.getTabLayout().getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
-                else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
-            }
         }
 
 //        setTheme(R.style.AppTheme2) ;
@@ -299,6 +300,51 @@ public class MainActivity extends AppCompatActivity implements
 //        int color = typedValue.data;
 
 
+
+    }
+
+    private void setUI(String status, String bar, String tabs, String tabu, String bg) {
+        if (!status.equals(""))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) window.setStatusBarColor(Integer.parseInt(status));
+        if (!bar.equals(""))
+            toolbar.setBackgroundColor(Integer.parseInt(bar));
+                GradientDrawable shape_no= (GradientDrawable) getResources().getDrawable(R.drawable.shape_no);
+        if (!tabu.equals(""))
+            shape_no.setColor(Integer.parseInt(tabu));
+                GradientDrawable shape_yes= (GradientDrawable) getResources().getDrawable(R.drawable.shape_yes);
+        if (!tabs.equals(""))
+                shape_yes.setColor(Integer.parseInt(tabs));
+        if (!bg.equals(""))
+                frameLayout.setBackgroundColor(Integer.parseInt(bg));
+    }
+
+    private void loadColorTab() {
+        try {
+            for (int i = 0; i < tabUI.getTabCount(); i++) {
+                if (tabUI.getTabAt(i).isSelected()){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                        tabUI.getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+                    else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
+                }else{
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                        tabUI.getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
+                    else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
+                }
+            }
+            for (int i = 0; i < fmDanhNgon.getTabLayout().getTabCount(); i++) {
+                if (fmDanhNgon.getTabLayout().getTabAt(i).isSelected()){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                        fmDanhNgon.getTabLayout().getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_yes));
+                    else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
+                }else{
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                        fmDanhNgon.getTabLayout().getTabAt(i).getCustomView().setBackground(getResources().getDrawable(R.drawable.shape_no));
+                    else Communication.showToastCenter(this,"Phiên bản hệ điều hành không hỗ trợ");
+                }
+            }
+        }catch (NullPointerException e){
+
+        }
 
     }
 }
